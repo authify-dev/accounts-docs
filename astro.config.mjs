86e9e07ac9bb64b9a5f051afc6aa8e875930b032
reitmas32 <rafa.zamora.rals@gmail.com>
2024-11-10 03:36:56 -0600

    feat: add custom header and add sidebar tree

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,23 +6,64 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'Docs Guide',
+			title: 'Accounts',
 			social: {
 				github: 'https://github.com/reitmas32/accounts',
 			},
+			defaultLocale: "en",
+			locales: {
+				en: {
+					label: 'English',
+				},
+				es: {
+					label: 'Español',
+				},
+			},
+			components: {
+				Header: "./src/components/CustomHeader.astro"
+			},
+
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+					label: 'Get Started',
+					autogenerate: { directory: 'get_started' },
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'API Reference',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{
+							label: 'Emails',
+							items: [
+								{
+									label: "SignUp",
+									slug: 'api_reference/emails/signup',
+								},
+								{
+									label: "Activate",
+									slug: 'api_reference/emails/activate',
+								},
+								{
+									label: "SignIn",
+									slug: 'api_reference/emails/signin',
+								},
+								{
+									label: "Resend Code",
+									slug: 'api_reference/emails/resend-code',
+								},
+								{
+									label: "Reset Password",
+									slug: 'api_reference/emails/reset-password',
+								}
+							]
+						},
+						{ label: 'Platforms', autogenerate: { directory: 'api_reference/platforms' } },
+						{ label: 'JWT', autogenerate: { directory: 'api_reference/jwt' } },
+					],
+
 				},
 			],
+
 		}),
 	],
 });
