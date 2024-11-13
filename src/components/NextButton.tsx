abb4ae7ec0485500092232204bd0507676f415dc
reitmas32 <rafa.zamora.rals@gmail.com>
2024-11-12 22:26:04 -0600

    feat: new deploy

--- src/components/NextButton.tsx
+++ src/components/NextButton.tsx
@@ -3,12 +3,12 @@ import {Snippet} from "@nextui-org/react";
 export default function App() {
   return (
     <div className="flex flex-wrap gap-4">
-      <Snippet color="default">npm install @nextui-org/react</Snippet>
-      <Snippet color="primary">npm install @nextui-org/react</Snippet>
-      <Snippet color="secondary">npm install @nextui-org/react</Snippet>
-      <Snippet color="success">npm install @nextui-org/react</Snippet>
-      <Snippet color="warning">npm install @nextui-org/react</Snippet>
-      <Snippet color="danger">npm install @nextui-org/react</Snippet>
+      <Snippet >npm install @nextui-org/react</Snippet>
+      <Snippet >npm install @nextui-org/react</Snippet>
+      <Snippet >npm install @nextui-org/react</Snippet>
+      <Snippet >npm install @nextui-org/react</Snippet>
+      <Snippet >npm install @nextui-org/react</Snippet>
+      <Snippet >npm install @nextui-org/react</Snippet>
     </div> 
   );
 }
